12a25392c6f74f6d11ef2836a8583ef0fe224d48
Againss <1079791280@qq.com>
2018-05-27 19:23:53 +0800

    first

---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="login">
+    <el-form ref="form" :model="form" class="container" :rules="rules">
+      <el-form-item>
+        <img src="../assets/Again.jpg" alt="" class="avatar">
+      </el-form-item>
+      <el-form-item prop='useName'>
+        <el-input v-model="form.useName" placeholder="账户" prefix-icon="iconfont icon-user-girl"></el-input>
+      </el-form-item>
+      <el-form-item prop='passWord'>
+        <el-input v-model="form.passWord" placeholder="密码" prefix-icon="iconfont icon-key"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" class="login-btn">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    form: {
+      useName: "",
+      passWord: ""
+    },
+    rules: {
+      useName: [{ required: true, message: "请输入账户名", trigger: "blur" }],
+      passWord: [{ required: true, message: "请输入密码", trigger: "blur" }]
+    }
+  }),
+  components: {}
+};
+</script>
+<style lang="less" scoped>
+.login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #2f4050;
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    background: white;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
+</style>
